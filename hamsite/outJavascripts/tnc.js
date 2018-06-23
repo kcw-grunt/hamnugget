ae52e1cee9e4e380b8e2e0dc1d7561e114714c9e
Kerry <mrkerrywashington@icloud.com>
2018-06-22 18:53:18 -0700

    added  serialport and it compiles

--- hamsite/outJavascripts/tnc.js
+++ hamsite/outJavascripts/tnc.js
@@ -1,4 +1,16 @@
+
+
 function check_test(){
     console.log("It's working! :D");
     return "It's working really!";
+}
+
+function test_connect(){
+    console.log("Connect!!!!!!"); 
+    var SerialPort = require('serialport');
+    
+
+
+
+
 }
