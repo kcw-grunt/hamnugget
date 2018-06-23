b1199d5234861e92d25e34ca44732ff619f009f2
Kerry <mrkerrywashington@icloud.com>
2018-06-22 20:24:28 -0700

    add echo route

--- hamsite/routes/index.js
+++ hamsite/routes/index.js
@@ -4,6 +4,10 @@ var SerialPort = require('serialport');
 var util = require('util');
 const ax25 = require('ax25');
  
+var myCallsign = "KM6TIG";
+var mySSID = 1;
+
+var sessions = {};
 
 router.get('/api/hello', (req, res) => {
   res.send({ response: 'World' });
@@ -29,16 +33,19 @@ router.get('/api/ham/tnc/on', (req, res) => {
   });
 });
 
-
 router.get('/api/ham/connect', (req, res) => {
-  // var SerialPort = require('serialport');
-  // var port1 = new SerialPort('/dev/ttyUSB0', function (err) {
-  //   if (err) {
-  //       return console.log('Error:', err.message);
-  //   } else {
-  //       return console.log("Connected to USB0"); 
-  //   }
-  // });
+
+  var port1 = new SerialPort('/dev/ttyUSB0', function (err) {
+    if (err) {
+        return console.log('Real Error:', err.message);
+    } else {
+        return console.log("Connected to USB0"); 
+    }
+  });
+
+});
+
+router.get('/api/ham/echo', (req, res) => {
 
   var tnc = new ax25.kissTNC(
     {	serialPort : "/dev/ttyUSB0",
@@ -46,91 +53,91 @@ router.get('/api/ham/connect', (req, res) => {
     }
   );
 
-      tnc.on(
-      "frame",
-      function(frame) {
+  tnc.on(
+    "frame",
+    function(frame) {
 
-        var packet = new ax25.Packet();
-        packet.disassemble(frame);
-        if( packet.destinationCallsign != myCallsign
-          ||
-          packet.destinationSSID != mySSID
-        ) {
-          return;
-        }
+      var packet = new ax25.Packet();
+      packet.disassemble(frame);
+      if( packet.destinationCallsign != myCallsign
+        ||
+        packet.destinationSSID != mySSID
+      ) {
+        return;
+      }
 
-        console.log(packet.log());
+      console.log(packet.log());
 
-        var clientID = util.format(
-          "%s-%s-%s-%s",
-          packet.sourceCallsign,
-          packet.sourceSSID,
-          packet.destinationCallsign,
-          packet.destinationSSID
+      var clientID = util.format(
+        "%s-%s-%s-%s",
+        packet.sourceCallsign,
+        packet.sourceSSID,
+        packet.destinationCallsign,
+        packet.destinationSSID
+      );
+
+      if(typeof sessions[clientID] == "undefined") {
+
+        sessions[clientID] = new ax25.Session();
+
+        sessions[clientID].on(
+          "packet",
+          function(frame) {
+            console.log(frame.log());
+            tnc.send(frame.assemble());
+          }
         );
 
-        if(typeof sessions[clientID] == "undefined") {
+        sessions[clientID].on(
+          "data",
+          function(data) {
+            sessions[clientID].sendString(
+              util.format(
+                "You sent: %s\r\n",
+                ax25.Utils.byteArrayToString(data)
+              )
+            );
+          }
+        );
 
-          sessions[clientID] = new ax25.Session();
+        sessions[clientID].on(
+          "connection",
+          function(state) {
+            console.log(
+              util.format(
+                "Client %s-%s %s.",
+                packet.sourceCallsign,
+                packet.sourceSSID,
+                (state) ? "connected" : "disconnected"
+              )
+            );
+            if(!state)
+              delete sessions[clientID];
+          }
+        );
 
-          sessions[clientID].on(
-            "packet",
-            function(frame) {
-              console.log(frame.log());
-              tnc.send(frame.assemble());
-            }
-          );
-
-          sessions[clientID].on(
-            "data",
-            function(data) {
-              sessions[clientID].sendString(
-                util.format(
-                  "You sent: %s\r\n",
-                  ax25.Utils.byteArrayToString(data)
-                )
-              );
-            }
-          );
-
-          sessions[clientID].on(
-            "connection",
-            function(state) {
-              console.log(
-                util.format(
-                  "Client %s-%s %s.",
-                  packet.sourceCallsign,
-                  packet.sourceSSID,
-                  (state) ? "connected" : "disconnected"
-                )
-              );
-              if(!state)
-                delete sessions[clientID];
-            }
-          );
-
-          sessions[clientID].on(
-            "error",
-            function(err) {
-              console.log(err);
-            }
-          );
-
-        }
-
-        if(typeof sessions[clientID] != "undefined")
-          sessions[clientID].receive(packet);
+        sessions[clientID].on(
+          "error",
+          function(err) {
+            console.log(err);
+          }
+        );
 
       }
 
-    );
+      if(typeof sessions[clientID] != "undefined")
+        sessions[clientID].receive(packet);
 
-    tnc.on(
-      "error",
-      function(err) {
-        console.log("HURRRRR! I DONE BORKED! " + err);
-      }
-    );
+    }
+
+  );
+
+  tnc.on(
+    "error",
+    function(err) {
+      console.log("HURRRRR! I DONE BORKED! " + err);
+    }
+  );
 
 });
 
